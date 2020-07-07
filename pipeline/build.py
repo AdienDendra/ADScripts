109c182523e588a544c3e49df706ace01f00f59f
adiendendra <adien.dendra@gmail.com>
2020-07-07 19:15:01 +0700

    initial commit

--- pipeline/build.py
+++ pipeline/build.py
@@ -15,37 +15,37 @@ reload(pr)
 reload(ct)
 reload(md)
 
-model_path        = '%s/assets/%s_ply.ma'
-builder_jnt_path   = '%s/builder/jointBiped_jnt.ma'
-skin_jnt_path      = '%s/builder/skinBiped_skin.ma'
-ctrl_path_hero     = '%s/release/%s_ctrl.ma'
+model_path        = '%s/assets/%s.ma'
+builder_biped_path   = '%s/builder/bipedTmp_jnt.ma'
+builder_quadruped_path   = '%s/builder/quadrupedTmp_jnt.ma'
 
-def_scene         = '%s/hero/%s_def.ma'
+# skin_jnt_path      = '%s/builder/skinBiped_skin.ma'
+# ctrl_path_hero     = '%s/release/%s_ctrl.ma'
+# def_scene         = '%s/hero/%s_def.ma'
 
+project_path = 'E:/Project/character'
 
 # import def file
-
-# defFile = defScene % (pr.mainPath(path), characterName)
-# mc.file(defFile, r=1, ns='%s_def' % characterName)
-
-def import_builder_file(project_path):
+def import_builder_file(character_mesh_name, project_name, biped=True):
 
     """
     main function to instance controller base
-
     """
     # new scene
     mc.file(new = True, f=True)
 
     # query character name of the path
 
-    # import builder jnt
-    builder_file = builder_jnt_path % (pr.path_base(project_path))
-    mc.file(builder_file, i = 1)
+    if biped:
+        # import builder jnt
+        builder_file = builder_biped_path % (project_path)
+        model_file = model_path % ((project_path + '/biped/%s' % project_name), character_mesh_name)
+    else:
+        builder_file = builder_biped_path % (builder_quadruped_path)
+        model_file = model_path % ((project_path + '/quadruped/%s' % project_name), character_mesh_name)
 
-    # reference the model
-    model_file = model_path % (project_path, pr.character_name(project_path))
-    mc.file (model_file, r=1, ns='%s_ply' % pr.character_name(project_path))
+    mc.file(builder_file, i = 1)
+    mc.file(model_file, i = 1)
 
 
 def csp_file(projectPath):
@@ -54,28 +54,28 @@ def csp_file(projectPath):
     main function to build control, skin, poly base
 
     """
-    # new scene
-    mc.file(new = True, f=True)
+    # # new scene
+    # mc.file(new = True, f=True)
+    #
+    # # reference ctrl
+    # ctrl_path = ctrl_path_hero % (projectPath, pr.character_name(projectPath))
+    #
+    # mc.file (ctrl_path, r=1, ns='%s_ctrl' % pr.character_name(projectPath))
 
-    # reference ctrl
-    ctrl_path = ctrl_path_hero % (projectPath, pr.character_name(projectPath))
-
-    mc.file (ctrl_path, r=1, ns='%s_ctrl' % pr.character_name(projectPath))
-
-    # import skin jnt
-    skin_path = skin_jnt_path % (pr.path_base(projectPath))
-    mc.file(skin_path, i = 1)
+    # # import skin jnt
+    # skin_path = skin_jnt_path % (pr.path_base(projectPath))
+    # mc.file(skin_path, i = 1)
 
     # import model
     model_file = model_path % (projectPath, pr.character_name(projectPath))
     mc.file (model_file, i=1)
 
-    # parent joint to Skin grp Base
-    mc.parent('rootSkin_jnt', baseRig.skinGrp)
-
-    # parent reference ctrl to Anim grp Base
-    ctrl_name = '%s_ctrl:partAnim_grp' % (pr.character_name(projectPath))
-    mc.parent(ctrl_name, baseRig.partGrp)
+    # # parent joint to Skin grp Base
+    # mc.parent('rootSkin_jnt', baseRig.skinGrp)
+    #
+    # # parent reference ctrl to Anim grp Base
+    # ctrl_name = '%s_ctrl:partAnim_grp' % (pr.character_name(projectPath))
+    # mc.parent(ctrl_name, baseRig.partGrp)
 
 def ref_model():
     # reference model
--- pipeline/directories.py
+++ pipeline/directories.py
@@ -2,8 +2,8 @@ import os
 
 import maya.cmds as mc
 
-sub_dir_list = ['assets', 'data', 'publish', 'release', 'scene']
-scene_dir_list = ['anim', 'bsh', 'build', 'csp', 'ctrl', 'master']
+sub_dir_list = ['assets', 'data', 'publish', 'scene']
+# scene_dir_list = ['anim', 'bsh', 'build', 'csp', 'ctrl', 'master']
 
 
 def create_dir(root_path, sub_dir_list):
@@ -24,33 +24,37 @@ def create_character_dir(root_path, new_dir_name):
         return mc.error("The directory %s already exist. Please change another name" % new_dir_name)
 
     res_dir = create_dir(new_path, sub_dir_list)
-    create_dir(res_dir.get('scene'), scene_dir_list)
+    # create_dir(res_dir.get('scene'), scene_dir_list)
 
 
-def create_directories(name='name'):
+def create_directories(project_name='name', biped=True):
     # change to character biped directory
-    os.chdir('E:/Project/characterBiped')
-    get_path = os.getcwd()
-
-    if not os.path.isdir(name):
-        os.mkdir(name)
+    if biped:
+        os.chdir('E:/Project/character/biped')
+        get_path = os.getcwd()
     else:
-        mc.error("The directory %s already exist. Please change another name" % name)
+        os.chdir('E:/Project/character/quadruped')
+        get_path = os.getcwd()
 
-    path_name = os.path.join(get_path, name)
+    if not os.path.isdir(project_name):
+        os.mkdir(project_name)
+    else:
+        mc.error("The directory %s already exist. Please change another name" % project_name)
+
+    path_name = os.path.join(get_path, project_name)
 
     # create all folder directories inside of name folder
     for folder in sub_dir_list:
         os.mkdir(os.path.join(path_name, folder))
 
-    # make scene folder inside of name object
-    os.mkdir(os.path.join(path_name, 'scene'))
+    # # make scene folder inside of name object
+    # os.mkdir(os.path.join(path_name, 'scene'))
 
-    # change to the scene directory
-    os.chdir('E:/Project/characterBiped/%s/scene' % name)
+    # # change to the scene directory
+    # os.chdir('E:/Project/character/biped/%s/scene' % name)
 
-    get_path_scene = os.getcwd()
+    # get_path_scene = os.getcwd()
 
-    # create all folder directoriesScene inside of scene folder
-    for folder in scene_dir_list:
-        os.mkdir(os.path.join(get_path_scene, folder))
+    # # create all folder directoriesScene inside of scene folder
+    # for folder in scene_dir_list:
+    #     os.mkdir(os.path.join(get_path_scene, folder))
