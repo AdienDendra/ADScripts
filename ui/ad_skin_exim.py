c7fb93c80fddea22f55c2453969b344128c029c6
adiendendra <adien.dendra@gmail.com>
2021-10-26 21:55:46 +0700

    initial commit

--- ui/ad_skin_exim.py
+++ ui/ad_skin_exim.py
@@ -13,43 +13,42 @@ import maya.cmds as cmds
 
 layout = 350
 percentage = 0.01 * layout
-on_selector = 0
-method_radio_button =1
+suffix = 'SkinWeight'
 
 def ad_show_ui():
-    adien_skin = 'AD_ExportImportSkin_Tool'
+    adien_skin = 'AD_Skin_Tool'
     pm.window(adien_skin, exists=True)
     if pm.window(adien_skin, exists=True):
         pm.deleteUI(adien_skin)
-    with pm.window(adien_skin, title='AD Export/Import Skin Tool', width=350, height=80):
-        # CREATE TAB
+    with pm.window(adien_skin, title='AD Skin Tool', width=350, height=80):
         with pm.columnLayout('Exim Tool', rowSpacing=1 * percentage, w=layout,
                              co=('both', 1 * percentage)):
             pm.separator(h=5, st="in", w=layout)
             with pm.rowLayout(nc=1):
                 pm.textFieldButtonGrp('Path', label='Path:', cal=(1, "right"),
-                                      cw3=(10 * percentage, 70 * percentage, 10 * percentage),
-                                      cat=[(1, 'right', 2), (2, 'both', 2), (3, 'left', 2)],
+                                      cw3=(10 * percentage, 68 * percentage, 18 * percentage),
+                                      cat=[(1, 'right', 2), (2, 'both', 1), (3, 'both', 1)],
                                       bl="Get Path",
                                       bc=partial(ad_get_path_button))
 
-            # ad_defining_object_text_field(define_object='Static_Joint', label="Static Joint:")
-            with pm.rowLayout(nc=3, cw3=(10 * percentage, 43.5 * percentage, 43.5 * percentage),
-                                  cat=[(1, 'right', 2), (2, 'both', 1), (3, 'both', 1)]):
+            with pm.rowLayout(nc=4, cw4=(10 * percentage, 34 * percentage, 34 * percentage, 18 * percentage) ,
+                                  cat=[(1, 'right', 2), (2, 'both', 2), (3, 'both', 2), (4, 'both', 1)]):
                 pm.text(l='')
                 pm.button('export_skin_weight', l="Export Skin Weight", bgc=(1, 1, 0),
                           c=partial(ad_export_skin_button))
 
-                # create button to delete last pair of text fields
                 pm.button("import_skin_weight", l="Import Skin Weight", bgc=(0, 0.5, 0),
-                          c=''
+                          c=partial(ad_import_skin_button)
+                          )
+
+                pm.button("transfer_skin_weight", l="Transfer", c=partial(ad_transfer_button)
                           )
             pm.separator(h=10, st="in", w=layout)
             with pm.rowLayout(nc=2, cw2=(49 * percentage, 49 * percentage), cl2=('center', 'center'),
                               columnAttach=[(1, 'both', 2 * percentage), (2, 'both', 2 * percentage)]):
                 pm.text(l='Adien Dendra | 11/2020', al='left')
                 pm.text(
-                    l='<a href="https://youtu.be/IqMGcvEzJCk">find out how to use it! >> </a>',
+                    l='<a href="mailto:adprojects.animation@gmail.com">email me! >> </a>',
                     hl=True,
                     al='right')
     pm.showWindow()
@@ -74,7 +73,7 @@ def ad_export_skin_button(*args):
                 if get_shape:
                     node_type =  pm.nodeType(get_shape)
                     if node_type == 'mesh' or node_type == 'nurbsSurface':
-                        ad_create_directories(item)
+                        ad_create_directories_export(item)
                     else:
                         om.MGlobal_displayWarning('The object type is not mesh. Skip object to export!')
                 else:
@@ -86,151 +85,274 @@ def ad_export_skin_button(*args):
     else:
         om.MGlobal_displayError('Set the path first!')
 
+def ad_import_skin_button(*args):
+    selection = pm.ls(sl=1)
+    object_path = pm.textFieldButtonGrp('Path', q=True, tx=True)
+    if object_path :
+        if selection:
+            for item in selection:
+                get_shape = item.getShape()
+                if get_shape:
+                    node_type =  pm.nodeType(get_shape)
+                    if node_type == 'mesh' or node_type == 'nurbsSurface':
+                        ad_read_increment_file_import(item)
+                    else:
+                        om.MGlobal_displayWarning('The object type is not mesh. Skip object to export!')
+                else:
+                    pass
+            pm.confirmDialog(title='Progress', message='Importing weight has done.')
 
-def ad_create_directories(item):
-    path = pm.textFieldButtonGrp('Path', q=True, tx=True)
-    # Export skin weight values into selected geometries
-    suffix = 'SkinWeight'
-    # Check whether the specified path exists or not
-
-    directory_sel = '%s/%s' % (path, item)
-    directory_exists = os.path.exists(directory_sel)
-
-    if not directory_exists:
-        # Create a new directory because it does not exist
-        os.mkdir('%s/%s' % (path, item))
-        directory = directory_sel
+        else:
+            om.MGlobal_displayError('Select the minimum one object mesh or nurbsSurface which has a skin weight!')
     else:
-        directory = directory_sel
+        om.MGlobal_displayError('Set the path first!')
 
-    # file_path = '%s/%s%s.json' % (directory, item, suffix)
+def ad_transfer_button(*args):
+    om.MGlobal.displayInfo('will be released soon!')
 
-    file_path = ad_create_increment_file(directory, item, suffix)
+# EXPORT
 
-    # for file in sorted(current_file_exists):
-    if file_path['total file'] > 10:
-        print file_path['current file exists'][0]
-        os.remove('%s/%s' % (directory, (file_path['current file exists'][0])))
+def ad_create_directories_export(item):
+    name = item.nodeName()
+    object = pm.ls(name)
+
+    if len(object) > 1:
+        om.MGlobal.displayWarning('Skin weight %s skipped! Due to duplicate object name' % item)
     else:
-        pass
+        path = pm.textFieldButtonGrp('Path', q=True, tx=True)
 
-    ad_data_skin_weight(item, file_path['file name'])
+        # Check whether the specified path exists or not
+        directory_sel = '%s/%s' % (path, item)
+        directory_exists = os.path.exists(directory_sel)
 
-    # if len(numb) > 10:
-    #     print
-    # os.remove('%s/%s.%s.%03d.json' % (directory_path, object_name, suffix, new_num))
-    # print num_list[2]
+        if not directory_exists:
+            # Create a new directory because it does not exist
+            os.mkdir('%s/%s' % (path, item))
+            directory = directory_sel
+        else:
+            directory = directory_sel
 
+        file_path = ad_increment_data_file(directory, item, suffix, create=True)
 
-def ad_create_increment_file(directory_path, object_name, suffix):
+        if file_path['total file'] > 10:
+            os.remove('%s/%s' % (directory, (file_path['current file exists'][0])))
+        else:
+            pass
+
+        ad_data_skin_weight_export(item, file_path['file name'])
+
+def ad_increment_data_file(directory_path, object_name, suffix, create):
     """
     increment the version update skin weight and deleting the older one
     """
-    current_file_exists = os.listdir(directory_path)
-    # prefix = 'SkinWeight'
-    current_file_exists = filter(lambda x: '.json' in x, current_file_exists)
-    num_list = [0]
-    for file in current_file_exists:
-        i = os.path.splitext(file)[0]
-        try:
-            num = re.findall('[0-9]+$', i)[0]
-            num_list.append(int(num))
-        except IndexError:
-            pass
-    num_list = sorted(num_list)
-    # print num_list
-    new_num = num_list[-1] + 1
-    total_file = len(num_list)
+    save_name, total_file, current_file_exists, get_file_path = [],[],[],[]
+    # for export
+    if create :
+        current_file_exists = os.listdir(directory_path)
+        current_file_exists = filter(lambda x: '.json' in x, current_file_exists)
+        num_list = [0]
+        for file in current_file_exists:
+            i = os.path.splitext(file)[0]
+            try:
+                num = re.findall('[0-9]+$', i)[0]
+                num_list.append(int(num))
+            except IndexError:
+                pass
+        num_list = sorted(num_list)
+        new_num = num_list[-1] + 1
+        total_file = len(num_list)
 
-    save_name = '%s/%s.%s.%03d.json' % (directory_path, object_name, suffix, new_num)
+        save_name = '%s/%s.%s.%03d.json' % (directory_path, object_name, suffix, new_num)
+        om.MGlobal.displayInfo('Export skin weight file as %s.%s.%03d.json' % (object_name, suffix, new_num))
 
-    # print "Saving %s" % save_name
+    # for import file
+    else:
+        get_file_path = sorted(os.listdir('%s/%s' % (directory_path, object_name)))
+
     return {'file name' : save_name,
             'total file': total_file,
-            'current file exists' : current_file_exists}
+            'current file exists' : current_file_exists,
+            'get file path': get_file_path}
 
-def ad_data_skin_weight(item, path):
+def ad_data_skin_weight_export(item, path):
     skin = mm.eval('findRelatedSkinCluster( "%s" )' % item)
     if skin:
-        print(path)
         method = cmds.getAttr('%s.skinningMethod' % skin)
         components = cmds.getAttr('%s.useComponents' % skin)
         influence = cmds.skinCluster(skin, q=True, inf=True)
         skin_set = cmds.listConnections('%s.message' % skin, d=True, s=False)[0]
 
-        # fid = open(fn, 'w')
-        weight_dict = {}
-
-        # shape_dict[str(item)] = {'influences': influence, 'name': skin, 'set': skin_set, 'skinningMethod': method,
-        #                                 'useComponents': components}
+        weight_dict = OrderedDict()
         weight_dict['influences'] = influence
         weight_dict['name'] = skin
         weight_dict['set'] = skin_set
         weight_dict['skinningMethod'] = method
         weight_dict['useComponents'] = components
+        # skin_values,  ranges =[],[]
+        # for number, item in enumerate (pm.polyEvaluate(item, v=True), influence):
+        #     print
 
         for range in xrange(pm.polyEvaluate(item, v=True)):
             current_vertex = '%s.vtx[%d]' % (item, range)
             skin_value = pm.skinPercent(skin, current_vertex, q=True, v=True)
-            weight_dict[range] = skin_value
+            value_with_object = zip(influence, skin_value)
+            # influence_percent = pm.skinCluster(skin, current_vertex, q=True, inf=True)
+            weight_dict[range] = value_with_object
 
+        # weight_dict['skin_value'] = skin_values
+        # weight_dict['range'] = ranges
         # write the json file
-
         file = open("%s" % (path), "w")
         json.dump(weight_dict, file, indent=4)
 
     else:
-        print('%s has no related skinCluster node.' % item)
+        om.MGlobal.displayWarning('%s has no related skinCluster node.' % item)
 
 
 
+# IMPORT
+def ad_read_increment_file_import(item):
+    path = pm.textFieldButtonGrp('Path', q=True, tx=True)
 
-def ad_exporting_skin_selected(item):
-    path = 'C:\Users\Raizel Dendra\Documents\maya\projects\default\scenes'
-    # Export skin weight values into selected geometries
-    suffix = 'Weight'
     # Check whether the specified path exists or not
+    directory_sel = '%s/%s' % (path, item)
+    directory_exists = os.path.exists(directory_sel)
 
-    file_path = '%s/%s%s' % (path, item, suffix)
-    return file_path
+    print os.listdir(directory_sel)
 
-    # ad_export_skin(item, file_path)
+    if not directory_exists:
+        # Create a new directory because it does not exist
+        om.MGlobal.displayWarning("Please check your object selection name. It doesn't match with directories exported!")
+    else:
+        if not os.listdir(directory_sel):
+            om.MGlobal.displayWarning("There is no skin %s exported. Import skipped!" % item)
+        else:
+            file_path = ad_increment_data_file(path, item, suffix, create=False)
 
-
-    # # all item shape in the list
-    # for item in list:
-    #     # get transform name
-    #     item_parent = item.getParent()
-    #
-    #     # get cv number, x value, y value, z value and color on each item
-    #     cvs, xvalue, yvalue, zvalue, color = [], [], [], [], []
-    #     for cv in pm.PyNode(item).cv:
-    #         # for cv in object_curve.getShape().cv:
-    #         x = pm.getAttr(str(cv) + '.xValue')
-    #         y = pm.getAttr(str(cv) + '.yValue')
-    #         z = pm.getAttr(str(cv) + '.zValue')
-    #         xvalue.append(x)
-    #         yvalue.append(y)
-    #         zvalue.append(z)
-    #
-    #         # get the number cv
-    #         cv = cv.split('.')[-1]
-    #         cvs.append(cv)
-    #
-    #     if pm.getAttr('%s.overrideEnabled' % item):
-    #         color_number = pm.getAttr('%s.overrideColor' % item)
-    #         color.append(color_number)
-    #     shape_dict[str(item_parent)] = {'cv': cvs, 'xValue': xvalue, 'yValue': yvalue, 'zValue': zvalue,
-    #                                     'overrideColor': color}
-    #
-    #     om.MGlobal.displayInfo("Object '%s' is saved!." % (item.getParent()))
-    #
-    # om.MGlobal.displayInfo("---------------- File path saved: '%s'" % file_name)
-    #
-    # # write the json file
-    # file = open("%s" % (file_name), "w")
-    # json.dump(shape_dict, file, indent=4)
+            ad_data_skin_weight_import(file_path['get file path'][-1], path, item)
 
 
+def ad_data_skin_weight_import(item_version, path, object_name):
+    print('Loading %s...' % object_name)
 
+    file = open("%s/%s/%s" % (path, object_name, item_version))
+    weight_dict = json.load(file)
+
+    influences = weight_dict['influences']
+
+    for influence in influences:
+        if not pm.objExists(influence):
+            om.MGlobal.displayWarning('Scene has no %s ' % influence)
+    related_skin = mm.eval('findRelatedSkinCluster "%s"' % object_name)
+    if related_skin:
+        pm.skinCluster(related_skin, e=True, ub=True)
+
+    create_skin = pm.skinCluster(influences[0], object_name, tsb=True)
+
+    for influence in influences[1:]:
+        influence_type = pm.objectType(influence)
+        if influence_type == 'joint':
+            pm.skinCluster(create_skin, e=True, ai=influence)
+        elif influence_type == 'transform':
+            base_influence = pm.duplicate(influence)[0]
+            pm.setAttr('%s.v' % base_influence, 0)
+            base_influence = pm.rename(base_influence, '%sBase' % base_influence)
+            shape = pm.listRelatives(base_influence, s=True, f=True, ni=True)[0]
+            pm.skinCluster(create_skin, e=True, ug=True, dr=4, ps=0, ns=10, wt=0,
+                           ai=influence, bsh=shape)
+        else:
+            pass
+
+    skin_attribute = pm.rename(create_skin, weight_dict['name'])
+    pm.setAttr('%s.skinningMethod' % skin_attribute, weight_dict['skinningMethod'])
+    pm.setAttr('%s.useComponents' % skin_attribute, weight_dict['useComponents'])
+
+    skin_set = pm.listConnections('%s.message' % skin_attribute, d=True, s=False)[0]
+    pm.rename(skin_set, weight_dict['set'])
+
+    # for influence in influences:
+    #     print influence
+    #     pm.setAttr('%s.liw' % influence, False)
+
+    pm.setAttr('%s.normalizeWeights' % skin_attribute, False)
+    pm.skinPercent(skin_attribute, object_name, nrm=False, prw=100)
+    pm.setAttr('%s.normalizeWeights' % skin_attribute, True)
+
+    vertex = pm.polyEvaluate(object_name, v=True)
+
+    keys = weight_dict.keys()
+
+    number_vertex = [number_vertex for number_vertex in keys if number_vertex.isdigit()]
+    number_vertex = sorted(map(int, number_vertex))
+
+    for vertex in number_vertex:
+        pm.skinPercent(skin_attribute, object_name+'.vtx[%s]' % vertex, transformValue=weight_dict[str(vertex)])
+
+    # print mynewlist
+    # print sorted(map(int, keys))
+
+    # numbers = []
+    # for number_vertex in keys:
+    #     if number_vertex.isdigit():
+    #         numbers.append(number_vertex)
+    #         pm.skinPercent(skin_attribute, object_name+'.vtx[%s]' % number_vertex, transformValue=weight_dict[number_vertex])
+
+
+            # print number_vertex, vertex
+            # if number_vertex == vertex:
+            #     om.MGlobal.displayInfo('100%% done.')
+            # else:
+            #     total_value = int((float(number_vertex) / vertex) * 100.00)
+            #     om.MGlobal.displayInfo('%s%% done.' % str(total_value))
+
+        #         counting = 0
+        #         if number_vertex > 0:
+        #             counting = int((float(number_vertex - 1) / vertex) * 100.00)
+        #
+        #         total_value = int((float(number_vertex) / vertex) * 100.00)
+        #
+        #         if not total_value == counting:
+        #             om.MGlobal.displayInfo('%s%% done.' % str(total_value))
+        # else:
+        #     pass
+        #
+        # list_influence = sorted(map(int, numbers))
+        # if number_vertex == list_influence[-1]:
+        #     om.MGlobal.displayInfo('100%% done.')
+
+
+
+#     om.MGlobal.displayInfo('100%% done.')
+
+
+            # for number_influence, influence in zip (weight_dict[number_vertex], influences):
+            #     print number_influence, influence
+            #     pm.skinPercent(skin_attribute, object_name+'.vtx[%s]' % number_vertex, transformValue=[])
+
+                # weight_attribute = '%s.weightList[%s].weights[%s]' % (skin_attribute, number_vertex, number_influence)
+                # pm.setAttr(weight_attribute, number_vertex)
+
+            # weight_value = weight_dict[item]
+
+    # for number_vertex in xrange(vertex):
+    #     for number_influence in xrange(len(influences)):
+    #         # print weight_dict[number_vertex]
+    #         weight_value = weight_dict[number_vertex][number_influence]
+    #         print weight_value
+
+                # if weight_value:
+
+        # # calculation
+        # number_vertex = int(number_vertex)
+        # if number_vertex == (vertex - 1):
+        #     om.MGlobal.displayInfo('100%% done.')
+        # else:
+        #     counting = 0
+        #     if number_vertex > 0:
+        #         counting = int((float(number_vertex - 1) / vertex) * 100.00)
+        #
+        #     total_value = int((float(number_vertex) / vertex) * 100.00)
+        #
+        #     if not total_value == counting:
+        #         om.MGlobal.displayInfo('%s%% done.' % str(total_value))
 
