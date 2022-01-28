504de1b6120d6f459e8a46732d1de6f91453dea5
adiendendra <adien.dendra@gmail.com>
2022-01-28 18:14:12 +1100

    initial commit

--- ui/ad_reset_attribute_node.py
+++ ui/ad_reset_attribute_node.py
@@ -2,13 +2,13 @@ import maya.cmds as mc
 import maya.mel as mel
 from functools import partial
 import maya.OpenMaya as om
-import ast
 
 MENU_NAME = "markingMenu"
-STRING_NAME ='_adNote'
 
 class markingMenu():
+
     def __init__(self):
+
         self.removeOld()
         self.build()
 
@@ -35,59 +35,69 @@ def ad_delete_all_default_attrs(*args):
                                       button=['Yes', 'No'], defaultButton='Yes',
                        cancelButton='No', dismissString='No')
     if confirm_dialog == 'Yes':
-        transform_listing = mc.ls()
-        for object in transform_listing:
-            node_type = mc.nodeType(object)
-            if node_type == 'nurbsCurve':
-                object = mc.listRelatives(object, p=1)[0]
-                ad_delete_attr(object)
-            elif node_type == 'joint':
-                ad_delete_attr(object)
-            else:
-                pass
+        transform_listing = mc.ls(type='transform')
+        ad_delete_node(transform_listing)
     else:
-        om.MGlobal.displayInfo("Cancel Delete All Default Attr!")
+        om.MGlobal.displayInfo("Cancel delete Define Attr Value!")
 
 
 # delete the _adNet node with selection
 def ad_delete_default_attr(*args):
     selection = mc.ls(sl=1)
-    for object in selection:
-        if not object:
-            om.MGlobal.displayWarning('Please select at least one object to Delete Default Attr!')
-        else:
-            ad_delete_attr(object)
+    if not selection:
+        om.MGlobal.displayWarning('Please select at least one object to Delete Attr Value!')
+    else:
+        ad_delete_node(selection)
 
 # deleting the node network
-def ad_delete_attr(selection):
-    # get the node name
-    attribute = mc.attributeQuery(STRING_NAME, node=selection, exists=True)
-    # if the node exists delete the node and attribute _adNet on object
-    if attribute:
-        # mc.delete(node)
-        mc.deleteAttr('%s.%s' % (selection, STRING_NAME))
-        om.MGlobal.displayInfo("Delete Default Attr %s" % selection)
-    else:
-        om.MGlobal.displayInfo('There is no attr value %s has been set! Skipped delete!' % selection)
+def ad_delete_node(selection):
+    for object in selection:
+        # get the node name
+        node = ad_reset_delete_query_attr(object)
+        # if the node exists delete the node and attribute _adNet on object
+        if node:
+            mc.delete(node)
+            mc.deleteAttr('%s._adNet' % object)
+            om.MGlobal.displayInfo("Delete Define Attr Value %s" % object)
+        else:
+            pass
+
 
 # reset the value attr
 def ad_reset_to_default(*args):
     selection = mc.ls(sl=1)
     if not selection:
-        om.MGlobal.displayWarning('Please select at least one object to Reset to Default!')
+        om.MGlobal.displayWarning('Please select at least one object to Reset Attr Value!')
     else:
         for object in selection:
-            # node = ad_reset_delete_query_attr(object)
-            attr_note = mc.attributeQuery(STRING_NAME, node=object, exists=True)
-
+            # get the node name
+            node = ad_reset_delete_query_attr(object)
             # if the node exists
-            if attr_note:
-                get_dic = mc.getAttr('%s.%s' % (object, STRING_NAME))
-                dict_encode = ast.literal_eval(get_dic)
-                for (key, value) in dict_encode.items():
-                    mc.setAttr('%s.%s' % (object, key), value)
+            if node:
+                # query all the attribute in node
+                list_attributes = mc.listAttr(node, ud=1)
+                # get the value on every attribute then set to the selection object
+                for attribute in list_attributes:
+                    get_value = mc.getAttr('%s.%s' % (node, attribute))
+                    mc.setAttr('%s.%s' % (object, attribute), get_value)
+                # else:
+                #     om.MGlobal.displayWarning("Reset %s skipped! Please set the 'Define Attr Value' first!" % object)
             else:
                 om.MGlobal.displayWarning("Reset %s skipped! Please set the 'Define Attr Value' first!" % object)
+
+# query the node for reset and delete function
+def ad_reset_delete_query_attr(selection):
+
+    attr_node = mc.attributeQuery("_adNet", node=selection, exists=True)
+    # check the connection exists
+    if attr_node:
+        # query the source node name
+        node = mc.listConnections(selection + '._adNet', s=True)[0]
+        return node
+
+    else:
+        om.MGlobal.displayInfo('There is no Define Attr Value exists to %s. Skipped the job!' % selection)
+
 
 # define attr value to marking menu
 def ad_set_default_attr(*args):
@@ -95,9 +105,19 @@ def ad_set_default_attr(*args):
     if selection:
         # For every node in selection #
         for item in selection:
-            node_name = item+'.'+ STRING_NAME
-            key_value_dic = ad_get_attr_value(item)
-            mc.setAttr(node_name, key_value_dic, type='string')
+            key_value_message = ad_get_attr_value(item)
+            node_name = item+'_adNet'
+            for (key, value) in key_value_message.items():
+                if mc.objExists(node_name):
+                    pass
+                else:
+                    # create node
+                    network_node = mc.createNode('network', n=node_name)
+                    mc.connectAttr(network_node+'.message', item+'._adNet')
+
+                # get the value and add to attr
+                ad_add_attr_node(node_name, attribute_name=key, attribute_type='double',
+                                 setAttr=True, attr=key, value=value)
     else:
         om.MGlobal.displayWarning('Please select at least one object to Define Attr Value!')
 
@@ -126,19 +146,14 @@ def ad_get_attr_value(selection):
     dic ={}
 
     # for node in selection:
-    attr_note = mc.attributeQuery(STRING_NAME, node=selection, exists=True)
-    if attr_note:
-        pass
-    else:
-        mc.addAttr(selection, ln=STRING_NAME, dt='string')
-        mc.setAttr(selection+'.'+STRING_NAME, e=True, k=True)
-    # ad_add_attr_node(selection, attribute_name='_adNet', attribute_type='string')
+    ad_add_attr_node(selection, attribute_name='_adNet', attribute_type='message')
 
     # If no channels are selected:
     if ad_channel_attr() == []:
         for attr in mc.listAnimatable(selection):
             # Sort out the actual name of just the attribute.
             attr = mc.ls(attr, sn=True)[0]
+            # print attr
             attr = attr.partition('.')[2]
             # get the value.
             get_value = mc.getAttr('%s.%s' % (selection, attr))
